ffd6fd4b03457ef3bd9675878f96bb68ecbfc93c
301083645 <67066443+301083645@users.noreply.github.com>
2020-08-15 15:54:56 -0400

    Add project files.

---
+++ HelpForAss5/DBViewForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelpForAss5
+{
+    public partial class DBViewForm : Form
+    {
+        public DBViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DBViewForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
